a4a42b434b6b51abbee50f458a8729d0375c0cf1
RodrickCS <caetanorodrigo46@gmail.com>
2023-10-04 09:05:10 -0300

    APSI

--- Python/APSI/script.py
+++ Python/APSI/script.py
@@ -4,7 +4,7 @@ import csv
 
 def obter_proximo_id():
     try:
-        with open("./APSI/usuarios.csv", "r", encoding="UTF8") as f:
+        with open("usuarios.csv", "r", encoding="UTF8") as f:
             reader = csv.reader(f)
             ids = [int(row[0]) for row in reader if len(row) >= 1]
             if ids:
@@ -17,14 +17,14 @@ def obter_proximo_id():
 
 def escrever_usuario_csv():
     try:
-        with open("./APSI/usuarios.csv", "a", encoding="UTF8", newline="") as f:
+        with open("usuarios.csv", "a", encoding="UTF8", newline="") as f:
             writer = csv.writer(f)
             nome = nome_label_field.get()
             email = email_label_field.get()
             proximo_id = obter_proximo_id()
             if proximo_id is not None:
                 writer.writerow([proximo_id, nome, email])  # Escreve o ID na primeira posição
-                print("Usuário escrito com sucesso no arquivo 'usuarios.csv' com ID:", proximo_id)
+                label_informacao.configure(text=f"Usuário escrito com sucesso no arquivo 'usuarios.csv' com ID: {proximo_id}")
             else:
                 print("Não foi possível obter o próximo ID.")
     except Exception as e:
@@ -35,26 +35,26 @@ def pesquisar_usuario():
         termo_pesquisa = termo_pesquisa_field.get()
         campo_pesquisa = campo_pesquisa_var.get()
         
-        with open("./APSI/usuarios.csv", "r", encoding="UTF8") as f:
+        with open("usuarios.csv", "r", encoding="UTF8") as f:
             reader = csv.reader(f)
             encontrado = False
             for row in reader:
                 if len(row) >= 3:
-                    if campo_pesquisa == "Nome" and termo_pesquisa.lower() == row[0].lower():
-                        print(f"Usuário encontrado: Código: {row[1]}, Nome: {row[0]}, Email: {row[2]}")
+                    if campo_pesquisa == "Nome" and termo_pesquisa.lower() == row[1].lower():
+                        label_informacao.configure(text=f"Código: {row[0]}, Nome: {row[1]}, Email: {row[2]}")
                         encontrado = True
                     elif campo_pesquisa == "Email" and termo_pesquisa.lower() == row[2].lower():
-                        print(f"Usuário encontrado: Código: {row[1]}, Nome: {row[0]}, Email: {row[2]}")
+                        label_informacao.configure(text=f"Código: {row[0]}, Nome: {row[1]}, Email: {row[2]}")
                         encontrado = True
             
             if not encontrado:
-                print(f"Nenhum usuário encontrado com {campo_pesquisa}: {termo_pesquisa}.")
+                label_informacao.configure(text=f"Nenhum usuário encontrado com o nome fornecido.")
     except Exception as e:
         print("Ocorreu um erro:", str(e))
 
 janela_principal = tk.Tk()
 janela_principal.title("APSI")
-janela_principal.geometry("300x250")
+janela_principal.geometry("400x300")
 
 janela_principal.columnconfigure(0, weight=1)
 janela_principal.columnconfigure(1, weight=1)
@@ -75,13 +75,13 @@ botao_cadastrar = Button(janela_principal, text="Clique para cadastrar", command
 botao_cadastrar.grid(column=1, row=3)
 
 termo_pesquisa_label = Label(janela_principal, text="Termo de Pesquisa:")
-termo_pesquisa_label.grid(column=0, row=4)
+termo_pesquisa_label.grid(column=0, row=5)
 
 termo_pesquisa_field = Entry()
 termo_pesquisa_field.grid(column=1, row=4)
 
 campo_pesquisa_label = Label(janela_principal, text="Campo de Pesquisa:")
-campo_pesquisa_label.grid(column=0, row=5)
+campo_pesquisa_label.grid(column=0, row=4)
 
 campo_pesquisa_var = StringVar(janela_principal)
 campo_pesquisa_var.set("Nome")  # Valor padrão
@@ -90,5 +90,8 @@ campo_pesquisa_menu.grid(column=1, row=5)
 
 botao_pesquisar = Button(janela_principal, text="Pesquisar", command=pesquisar_usuario)
 botao_pesquisar.grid(column=1, row=6)
+
+label_informacao = Label(janela_principal, text="")
+label_informacao.grid(column=0, row=7)
 
 janela_principal.mainloop()
