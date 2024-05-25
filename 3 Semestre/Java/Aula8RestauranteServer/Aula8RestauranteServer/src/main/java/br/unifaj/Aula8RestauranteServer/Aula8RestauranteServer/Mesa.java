13c841fb2706a8e9730cbb6ade3718a2aa2b4b0c
RodrickCS <caetanorodrigo46@gmail.com>
2024-05-25 12:45:08 -0300

    Alterações

---
+++ 3 Semestre/Java/Aula8RestauranteServer/Aula8RestauranteServer/src/main/java/br/unifaj/Aula8RestauranteServer/Aula8RestauranteServer/Mesa.java
@@ -0,0 +1,4 @@
+package br.unifaj.Aula8RestauranteServer.Aula8RestauranteServer;
+
+public class Mesa {
+}
---
+++ 3 Semestre/Java/Aula8RestauranteServer/Aula8RestauranteServer/src/test/java/br/unifaj/Aula8RestauranteServer/Aula8RestauranteServer/Aula8RestauranteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unifaj.Aula8RestauranteServer.Aula8RestauranteServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula8RestauranteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
